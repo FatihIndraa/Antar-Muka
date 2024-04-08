3470813e47dba743aa41c04eeabee8b517284c58
FatihIndraa <fatihindra140903@gmail.com>
2024-04-08 07:16:06 +0700

    update fix

--- app/src/main/java/com/example/antarmuka/HalamanActivity.java
+++ app/src/main/java/com/example/antarmuka/HalamanActivity.java
@@ -1,6 +1,9 @@
 package com.example.antarmuka;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,25 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HalamanActivity extends AppCompatActivity {
+    Button buttonHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_halaman);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.halaman), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        buttonHome = findViewById(R.id.button_home);
+        buttonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HalamanActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/com/example/antarmuka/MainActivity.java
+++ app/src/main/java/com/example/antarmuka/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.antarmuka;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        buttonNext = findViewById(R.id.button_next);
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HalamanActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
